8628c956a683f61f5748c508d8c168291cf3a3a2
Midhin-17 <1728midhin@gmail.com>
2023-07-28 18:30:14 +0530

    This is a express setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("express setup");
+
+const express = require('express')
+const app = express()
+const PORT= 4000;
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(4000,() => console.log(` Listening on the ${PORT}`));
